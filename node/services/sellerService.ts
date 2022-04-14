b081a32437ed4211790166dd26b789f5e965121e
Wei Huan Chen <wei.chen@vtex.com.br>
2022-04-14 11:01:02 -0400

    [ADYENPLAT-36] changed for loop to map and removed lint disable

--- node/services/sellerService.ts
+++ node/services/sellerService.ts
@@ -4,47 +4,50 @@ export default {
   all: async ({ ctx }: { ctx: Context }) => {
     const sellers = await ctx.clients.sellersClient.sellers()
     const adyenAccounts = await ctx.clients.account.all()
-
-    const response = sellers.reduce((prev, seller) => {
-      if (seller.account === ctx.vtex.account) return prev
-
-      let adyenAccount: SellerAccount | null = null
-
-      for (const account of adyenAccounts) {
-        if (
-          account.sellerId === seller.id &&
-          (!adyenAccount || account.status === 'Active')
-        ) {
-          adyenAccount = account
-          if (account.status === 'Active') {
-            break
-          }
-        }
-      }
-
-      prev.push({
-        ...seller,
-        adyenAccount,
-      })
-
-      return prev
-    }, [] as any)
-
     const settingsFetch = await settings(ctx)
 
-    for (const seller of response) {
-      if (seller?.adyenAccount) {
-        // eslint-disable-next-line no-await-in-loop
-        const accountStatus = await ctx.clients.adyenClient.getAccountHolder(
-          seller?.adyenAccount?.accountHolderCode,
-          settingsFetch
-        )
+    const response = await Promise.all(
+      sellers
+        .reduce((prev, seller) => {
+          if (seller.account === ctx.vtex.account) return prev
 
-        seller.adyenAccount.status =
-          accountStatus?.accountHolderStatus.status ??
-          seller.adyenAccount.status
-      }
-    }
+          let adyenAccount: SellerAccount | null = null
+
+          for (const account of adyenAccounts) {
+            if (
+              account.sellerId === seller.id &&
+              (!adyenAccount || account.status === 'Active')
+            ) {
+              adyenAccount = account
+              if (account.status === 'Active') {
+                break
+              }
+            }
+          }
+
+          prev.push({
+            ...seller,
+            adyenAccount,
+          })
+
+          return prev
+        }, [] as any)
+        .map(async (seller: any) => {
+          if (seller?.adyenAccount) {
+            const accountStatus =
+              await ctx.clients.adyenClient.getAccountHolder(
+                seller?.adyenAccount?.accountHolderCode,
+                settingsFetch
+              )
+
+            seller.adyenAccount.status =
+              accountStatus?.accountHolderStatus.status ??
+              seller.adyenAccount.status
+          }
+
+          return seller
+        })
+    )
 
     return response
   },
